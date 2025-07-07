4d0f5c0dbf92d522cb3fba7ba94348926ebb780d
Saad <msncoder1999@gmail.com>
2025-07-08 00:08:32 +0500

    make a project with name dms project, make inside app of dms project which name is documents make a model which name is Document inside models.py file

---
+++ documents/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Document(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    file = models.FileField(upload_to='documents/')
+    summary = models.TextField(blank=True,null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    
