555c560a9d8310370710eece9b805c7fd1160ae9
Saad <msncoder1999@gmail.com>
2025-07-08 00:21:11 +0500

    migration and first document add

--- documents/models.py
+++ documents/models.py
@@ -7,11 +7,10 @@ class Document(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     category = models.CharField(max_length=100)
-    file = models.FileField(upload_to='documents/')
+    file = models.FileField(upload_to='documents/docx/')
     summary = models.TextField(blank=True,null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-    
     
