3a3556c42dcfabde7ce6b0a3c9cba36bdadccd14
Saad <msncoder1999@gmail.com>
2025-07-14 01:27:39 +0500

    urls.py file complete

--- documents/urls.py
+++ documents/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('list/', views.document_list, name='document_list'),  # ✅ Add this
     # path('ajax-search/', views.ajax_search_documents, name='ajax_search_documents'),
     path('document/<int:pk>/', views.document_detail, name='document_detail'),
-    
-
+    path('document/<int:pk>/', views.delete_document, name='delete_document'),
+    path('register/',views.register,name='register'),
+    path('login/',views.user_login,name='login'),
+    path('logout/',views.user_logout,name='logout'),
 ]
